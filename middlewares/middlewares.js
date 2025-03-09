b30851ceca25ca66a6dd0aaffff5e55d0442e235
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2025-03-09 20:02:57 -0300

    Update middlewares.js

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -1,15 +1,15 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const verifyToken = (req, res, next) => {
-    const token = req.headers['x-access-token'];
-    if (!token) {
-        return res.status(401).json({ auth: false, message: 'No token provided' });
-    }
-    const decoded = jwt.verify(token, process.env.SECRET_KEY);
-    req.userId = decoded.id;
-    console.log('Petición recibida con éxito');
-    next();
+const verificarToken = (req, res, next) => {
+    const token = req.headers.authorization;
+    if (!token) return res.status(401).json({ message: 'Acceso denegado' });
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) return res.status(401).json({ message: 'Token inválido' });
+        req.userEmail = decoded.email;
+        next();
+    });
 }
 
 
- module.exports = verifyToken; 
+ module.exports = verificarToken; 
