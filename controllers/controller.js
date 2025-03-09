f0da5f690cb6dcd1df39e90997edb8de8829949c
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2025-03-09 20:21:34 -0300

    reparacion controller con jwt y bcrypt

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,35 +1,52 @@
 const pool = require('../config/db');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 //ruta login 
  const softJobsLogin = async (req, res) => {
     const { email, password } = req.body;
-    const response = await pool.query('SELECT * FROM usuarios WHERE email = $1', [email]);
-    const user = response.rows[0];
-    if (user) {
-        const valid = await bcrypt.compare(password, user.password);
-        if (valid) {
-            const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY);
-            return res.json({ auth: true, token });
-        }
+    try {
+        const result = await pool.query('SELECT * FROM usuarios WHERE email = $1', [email]);
+        if (result.rows.length === 0) return res.status(400).json({ message: 'Tu usuario o contraseña es incorrecto' });
+        const user = result.rows[0];
+        const Encriptado = await bcrypt.compare(password, user.password);
+        if (!Encriptado) return res.status(400).json({ message: 'Tu usuario o contraseña es incorrecto' });
+
+        const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET, {
+            expiresIn: 60 * 60 * 24
+        });
+        res.json({ token });
+    } catch (error) {
+            res.status(500).json({ message: 'Error en el servidor' }
+
+        )
     }
-    return res.status(400).json({ auth: false });
-}
+};
 
 //ruta registrarse
  const softJobsRegister = async (req, res) => {
-    const { name, email, password } = req.body;
-    const response = await pool.query('INSERT INTO usuarios (name, email, password) VALUES ($1, $2, $3)', [name, email, password]);
-    if (response) {
-        return res.json({ auth: true });
-    }
-    return res.status(400).json({ auth: false });
-}
+    const { email, password, rol, lenguage } = req.body;
+    //const response = await pool.query('INSERT INTO usuarios (name, email, password) VALUES ($1, $2, $3)', [name, email, password]);
+    const hashedPassword = await bcrypt.hash(password, 10);
+    try {
+        await pool.query('INSERT INTO usuarios (email, password, rol, lenguage) VALUES ($1, $2, $3, $4)', [email, hashedPassword, rol, lenguage]);
+        res.json({ message: 'Usuario creado correctamente' });
+    } catch (error) {
+        res.status(500).json({ message: 'Error al registrar usuario' });  
 
-// ruta usuarios 
- const softJobsProfile = async (req, res) => {
-    const response = await pool.query('SELECT * FROM usuarios WHERE id = $1', [req.userId]);
-    return res.json(response.rows[0]);
-}
+}};
+
+// ruta usuario logueado
+    const softJobsProfile = async (req, res) => {
+        const email = req.userEmail;
+        try {
+            const result = await pool.query('SELECT * FROM usuarios WHERE email = $1', [email]);
+            const user = result.rows[0];
+            res.json({ email: user.email, rol: user.rol, lenguage: user.lenguage });
+        } catch (error) {
+            res.status(500).json({ message: 'Error en el servidor' });
+        }
+    };
 
 module.exports = { softJobsLogin, softJobsRegister, softJobsProfile
 }
